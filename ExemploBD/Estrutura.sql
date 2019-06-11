d60b524b10458ce74fc077e688108d7f8eb56c95
ElianNasatto <elian.nasa123@gmail.com>
2019-06-11 11:11:21 -0300

    Criado Banco de Dados

---
+++ ExemploBD/Estrutura.sql
@@ -0,0 +1,7 @@
+CREATE TABLE carro(
+ id INT PRIMARY KEY IDENTITY(1,1),
+ cor VARCHAR(50),
+ modelo VARCHAR(80),
+ preco DECIMAL(6,2),
+ ano INT
+);
---
+++ ExemploBD/Exemplo00/Carro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo00
+{
+    class Carro
+    {
+        public string Cor;
+        public string Modelo;
+        public decimal Preco;
+        public int Ano;
+    }
+}
